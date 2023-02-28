76cd971531f808316391a12202ef1ecf4aeb4036
Erik Enriquez <32780885+emenriquez@users.noreply.github.com>
2023-02-27 18:40:34 -0600

    Updated with Node Constructor

--- LLStack.cpp
+++ LLStack.cpp
@@ -1,17 +1,26 @@
 #include "LLStack.h"
 
+// Node constructor: initialize new nodes with an input string s
+	// s should be stored as the data of the node
+	// the nodes are not initially linked to any other node
+	// so be sure that the "next" Node is linked to a nullptr
+Node::Node(string s) 
+{
+
+}
+
 //constructor : initiazlize the head and tail field from LLStack class 
-// the stack should start with no items 
-// the head and tail should both be initialized as null pointers
+	// the stack should start with no items 
+	// the head and tail should both be initialized as null pointers
 LLStack::LLStack()
 {
 
 }
 
 /*
-define the top() method, which will return the data at the top of the stack
-(remember the "top" of the stack is the newest element)
-If stack is empty, return "";
+	define the top() method, which will return the data at the top of the stack
+	(remember the "top" of the stack is the newest element)
+	If stack is empty, return "";
 */
 string LLStack::top()
 {
@@ -19,7 +28,7 @@ string LLStack::top()
 }
 
 /*
-define the size() method, which will return the number of nodes in the stack
+	define the size() method, which will return the number of nodes in the stack
 */
 int LLStack::size()
 {
@@ -27,12 +36,12 @@ int LLStack::size()
 }
 
 /*
-- Create the new Node and initialize the fields of class Node (data and next)
-- update the head, tail and count accordingly
+	- Create the new Node and initialize the fields of class Node (data and next)
+	- update the head, tail and count accordingly
 
-Hint: To update head and tail properly, You have to consider two scenarios:
-1. If there is no element in the stack and this is the first one going to the stack
-2. If there is another head in the stack
+	Hint: To update head and tail properly, You have to consider two scenarios:
+	1. If there is no element in the stack and this is the first one going to the stack
+	2. If there is another head in the stack
 */
 void LLStack::push(string s)
 {
@@ -40,11 +49,11 @@ void LLStack::push(string s)
 }
 
 /*
-First, Check if the stack is empty or not. If not, update the head,tail and count accordingly.
-Don't forget to release memory using delete
-Hint: To update head and tail properly, You should consider two different scenario:
-1. If LLStack has more than one element
-2. If LLStack has exactly one element
+	First, Check if the stack is empty or not. If not, update the head,tail and count accordingly.
+		*Don't forget to release memory using delete
+	Hint: To update head and tail properly, You should consider two different scenarios:
+	1. If LLStack has more than one element
+	2. If LLStack has exactly one element
 */
 void LLStack::pop()
 {
